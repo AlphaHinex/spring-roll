e8af65b4f64720f90e057fe8c5467c58cd4dd200
Alpha Hinex <AlphaHinex@gmail.com>
2019-10-01 22:52:32 +0800

    Add test cases

--- modules/raw-materials/roll-web/src/main/java/io/github/springroll/web/model/DataTrunk.java
+++ modules/raw-materials/roll-web/src/main/java/io/github/springroll/web/model/DataTrunk.java
@@ -1,6 +1,7 @@
 package io.github.springroll.web.model;
 
 import io.github.springroll.Version;
+import io.github.springroll.utils.CollectionUtil;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
@@ -10,7 +11,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 /**
- * 数据仓库实体
+ * 数据仓库对象
  * 用来存放结果集数据及集合相关基本信息
  * 与 Spring Data 提供的 Page 接口定位类似，单独定义以不直接依赖 Spring Data
  * 可在返回集合的方法上，定义 JsonView 以控制数据展示视角
@@ -22,35 +23,35 @@ public class DataTrunk<T> implements Serializable {
     public static final long serialVersionUID = Version.HASH;
 
     /**
-     * 结果集数据
+     * 结果集数据，可以是全部数据集合，也可以是部分数据集合
      */
     private Collection<T> data;
 
     /**
      * 结果集数据总数
      */
-    private long count;
+    private long total;
 
     public DataTrunk() {
     }
 
     public DataTrunk(Collection<T> data) {
         this.data = data;
-        this.count = null == data ? 0 : data.size();
+        this.total = null == data ? 0 : data.size();
     }
 
-    public DataTrunk(Collection<T> data, long count) {
+    public DataTrunk(Collection<T> data, long total) {
         this.data = data;
-        this.count = count;
+        this.total = total;
     }
 
     @SuppressWarnings("unchecked")
     public DataTrunk(Collection<T> allData, int pageNo, int pageSize) {
         if (CollectionUtils.isEmpty(allData)) {
             this.data = Collections.EMPTY_LIST;
-            this.count = 0;
+            this.total = 0;
         } else {
-            Assert.isTrue(pageNo > 0, "Page Number SHOULD FROM 1!");
+            Assert.isTrue(pageNo > 0 && pageSize > 0, "Page number and page size SHOULD FROM 1!");
             int from = (pageNo - 1) * pageSize;
             int to = from + pageSize;
             if (to > allData.size()) {
@@ -58,12 +59,26 @@ public class DataTrunk<T> implements Serializable {
             }
             if (from > to) {
                 this.data = Collections.EMPTY_LIST;
-                this.count = allData.size();
+                this.total = allData.size();
             } else {
                 this.data = Arrays.asList((T[]) Arrays.copyOfRange(allData.toArray(), from, to));
-                this.count = allData.size();
+                this.total = allData.size();
             }
         }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof DataTrunk)) {
+            return false;
+        }
+        DataTrunk dt = (DataTrunk) obj;
+        return CollectionUtil.isEqualCollection(dt.getData(), data) && dt.getTotal() == total;
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode() + ((Long)total).hashCode();
     }
 
     public Collection<T> getData() {
@@ -74,12 +89,12 @@ public class DataTrunk<T> implements Serializable {
         this.data = data;
     }
 
-    public long getCount() {
-        return count;
+    public long getTotal() {
+        return total;
     }
 
-    public void setCount(long count) {
-        this.count = count;
+    public void setTotal(long total) {
+        this.total = total;
     }
 
 }
--- modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/model/DataTrunkSpec.groovy
+++ modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/model/DataTrunkSpec.groovy
@@ -4,15 +4,15 @@ import spock.lang.Specification
 
 class DataTrunkSpec extends Specification {
 
-    def "Sub collection"() {
+    def "Check size and total with page no and size"() {
         DataTrunk dt = new DataTrunk<>(allData, pageNo, pageSize)
 
         expect:
         dt.data.size() == size
-        dt.count == count
+        dt.total == total
 
         where:
-        pageNo | pageSize | size | count | allData
+        pageNo | pageSize | size | total | allData
         1      | 10       | 3    | 3     | ['a', 'b', 'c']
         2      | 2        | 2    | 4     | ['a', 'b', 'c', 'd']
         1      | 2        | 2    | 5     | ['a', 'b', 'c', 'd', 'e']
@@ -22,5 +22,37 @@ class DataTrunkSpec extends Specification {
         2      | 2        | 0    | 0     | []
         2      | 2        | 0    | 0     | null
     }
+
+    def "Check constructors"() {
+        def data = ['a','b']
+        DataTrunk dt1 = new DataTrunk()
+        dt1.setData(data)
+        dt1.setTotal(data.size())
+
+        DataTrunk dt2 = new DataTrunk(data)
+
+        DataTrunk dt3 = new DataTrunk(data, data.size())
+
+        DataTrunk dt4 = new DataTrunk(null)
+
+        expect:
+        dt1 == dt2
+        dt2 == dt3
+        dt1 == dt3
+        dt1.hashCode() == dt2.hashCode()
+        dt1.hashCode() == dt3.hashCode()
+        dt4.getTotal() == 0
+        dt1 != data
+
+        dt3.setTotal(10)
+        dt1 != dt3
+
+        dt2.setData(null)
+        dt1 != dt2
+    }
+
+//    def "Check equal"() {
+//
+//    }
 
 }
