db506a3539959cdfcebb91bef83a098d2dfcf2ac
Alpha Hinex <AlphaHinex@gmail.com>
2019-10-23 20:51:14 +0800

    Change package

--- modules/dev-kits/roll-dev-configs/src/test/resources/io/github/springroll/dev/configs/checkstyle/BeFiltered.java
+++ modules/dev-kits/roll-dev-configs/src/test/resources/io/github/springroll/dev/configs/checkstyle/BeFiltered.java
@@ -1,8 +1,11 @@
-package com.proper.checkstyle;
+package io.github.springroll.dev.configs.checkstyle;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * Not to be checked
+ */
 public class BeFilterd {
 
     @ApiOperation("blah blah")
