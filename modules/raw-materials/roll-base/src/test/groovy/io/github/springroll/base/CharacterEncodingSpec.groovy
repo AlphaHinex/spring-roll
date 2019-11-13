d9e0c6526325047efc66f246f38d177caaf4113b
Alpha Hinex <AlphaHinex@gmail.com>
2019-11-13 21:02:58 +0800

    100% coverage

---
+++ modules/raw-materials/roll-base/src/test/groovy/io/github/springroll/base/CharacterEncodingSpec.groovy
@@ -0,0 +1,16 @@
+package io.github.springroll.base
+
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+class CharacterEncodingSpec extends Specification {
+
+    def 'test'() {
+        expect:
+        CharacterEncoding.getCharset() == StandardCharsets.UTF_8
+        CharacterEncoding.setCharset(StandardCharsets.ISO_8859_1)
+        CharacterEncoding.getCharset() == StandardCharsets.ISO_8859_1
+    }
+
+}
