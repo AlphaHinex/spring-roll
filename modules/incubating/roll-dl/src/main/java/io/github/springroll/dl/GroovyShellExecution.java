c1fcfdd3406e02efeef1bccc653c081db638a764
Alpha Hinex <AlphaHinex@gmail.com>
2019-12-07 23:18:46 +0800

    Try lombok

--- modules/incubating/roll-dl/src/main/java/io/github/springroll/dl/GroovyShellExecution.java
+++ modules/incubating/roll-dl/src/main/java/io/github/springroll/dl/GroovyShellExecution.java
@@ -7,10 +7,9 @@ import groovy.lang.GroovyShell;
 import io.github.springroll.base.CharacterEncoding;
 import io.github.springroll.utils.StringUtil;
 import io.github.springroll.utils.digest.Md5;
+import lombok.extern.slf4j.Slf4j;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.runtime.InvokerHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,10 +29,9 @@ import java.util.stream.Stream;
  * 本运行环境的一个主要使用场景是校验，通过动态脚本或类，使校验规则能够灵活定义并动态加载
  * 校验时应尽量使用读操作，避免写操作，以免对共享数据对象造成改变及预期之外的影响
  */
+@Slf4j
 @Component
 public class GroovyShellExecution {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GroovyShellExecution.class);
 
     /**
      * 按照"与"的方式，组合脚本执行结果
@@ -122,7 +120,7 @@ public class GroovyShellExecution {
             Class scriptClass = getScriptClass(scriptContent);
             return InvokerHelper.createScript(scriptClass, binding).run();
         } catch (Exception e) {
-            LOGGER.error("Execute script ERROR!\r\nScript: {}", scriptContent, e);
+            log.error("Execute script ERROR!\r\nScript: {}", scriptContent, e);
             throw new GroovyScriptException(e);
         }
     }
@@ -153,10 +151,10 @@ public class GroovyShellExecution {
         }
 
         if (classCache.containsKey(key)) {
-            LOGGER.trace("Found key [{}] with value [{}] from cache, use cached Script object.", key, val);
+            log.trace("Found key [{}] with value [{}] from cache, use cached Script object.", key, val);
             return classCache.get(key);
         }
-        LOGGER.debug("Could NOT find key [{}] with value [{}] from cache, create a new one.", key, val);
+        log.debug("Could NOT find key [{}] with value [{}] from cache, create a new one.", key, val);
 
         GroovyCodeSource gcs;
         if (obj instanceof String) {
