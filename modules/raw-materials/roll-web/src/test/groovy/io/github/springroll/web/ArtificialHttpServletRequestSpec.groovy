bd19d03fe58de42ca7c57cf1051faa1730a54a8d
Alpha Hinex <AlphaHinex@gmail.com>
2020-03-06 09:19:17 +0800

    [IMP] ArtificialHttpServletRequest support to operate attributes (#26)
    1. 所有未实现的方法抛出 `UnsupportedOperationException ` 异常，与 javadoc 中描述保持一致，方便尽快感知 ArtificialHttpServletRequest 中可能会被用到、需要实现的方法
    2. 在实现了 attributes 相关方法后，支持了通用导出 Excel 功能实际调用的业务方法接口中包括 `@PathVariable` 的情况

--- modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/ArtificialHttpServletRequestSpec.groovy
+++ modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/ArtificialHttpServletRequestSpec.groovy
@@ -16,7 +16,7 @@ class ArtificialHttpServletRequestSpec extends Specification {
 
 
     def setup() {
-        request.setParams(params)
+        request.setParameters(params)
     }
 
     def 'check useful getters'() {
@@ -72,6 +72,18 @@ class ArtificialHttpServletRequestSpec extends Specification {
         'application/json'               | 'application/json'               | null
     }
 
+    def 'operate attribute'() {
+        request.setAttribute('a', '1')
+        request.setAttribute('b', '2')
+        request.setAttribute('b', null)
+
+        expect:
+        request.getAttribute('a') == '1'
+        request.getAttribute('b') == null
+        request.removeAttribute('a')
+        request.getAttribute('a') ==  null
+    }
+
     def 'not support methods'() {
         def exclude = [
                 'getContextPath',
@@ -93,8 +105,12 @@ class ArtificialHttpServletRequestSpec extends Specification {
                 'getHeader',
                 'getHeaders',
                 'getHeaderNames',
+                'setCharacterEncoding',
                 'getCharacterEncoding',
-                'setParams',
+                'setParameters',
+                'removeAttribute',
+                'getAttribute',
+                'setAttribute',
                 '$jacocoInit'
         ]
 
@@ -109,16 +125,11 @@ class ArtificialHttpServletRequestSpec extends Specification {
             } else {
                 Arrays.fill(params, null)
             }
-            switch (method.getReturnType().getSimpleName()) {
-                case 'boolean':
-                    method.invoke(request, params) == false
-                    break
-                case 'long':
-                case 'int':
-                    method.invoke(request, params) == 0
-                    break
-                default:
-                    method.invoke(request, params) == null
+
+            try {
+                method.invoke(request, params)
+            } catch (Exception e) {
+                true
             }
         }
     }
