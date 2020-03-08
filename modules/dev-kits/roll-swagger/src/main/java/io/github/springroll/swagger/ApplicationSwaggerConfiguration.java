4493794b4a73baea37e310764daba3848213a4bf
Alpha Hinex <AlphaHinex@gmail.com>
2020-03-08 12:44:30 +0800

    Compile pass

--- modules/dev-kits/roll-swagger/src/main/java/io/github/springroll/swagger/ApplicationSwaggerConfiguration.java
+++ modules/dev-kits/roll-swagger/src/main/java/io/github/springroll/swagger/ApplicationSwaggerConfiguration.java
@@ -1,6 +1,6 @@
 package io.github.springroll.swagger;
 
-import org.apache.commons.collections.ListUtils;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
