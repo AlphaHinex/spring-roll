b74eaddd7499d56445f03f06dda3d2d3f08bb7e7
Alpha Hinex <AlphaHinex@gmail.com>
2020-04-04 15:04:37 +0800

    For coverage

--- modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/request/InvokeControllerByRequestTest.groovy
+++ modules/raw-materials/roll-web/src/test/groovy/io/github/springroll/web/request/InvokeControllerByRequestTest.groovy
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
+
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
 
 class InvokeControllerByRequestTest extends AbstractSpringTest {
 
@@ -26,12 +30,27 @@ class InvokeControllerByRequestTest extends AbstractSpringTest {
         assert res instanceof ResponseEntity
     }
 
+    @Test(expected = ServletException)
+    void noAdapterForHandler() {
+        overrideSingleton('requestMappingHandlerAdapter', new AlwaysFalseRequestMappingHandlerAdapter())
+        def request = new ArtificialHttpServletRequest('', '', '/service/handler/mapping')
+        invokeControllerByRequest.invoke(request)
+    }
+
+}
+
+@Component
+class WithoutHandlerRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+    @Override
+    protected HandlerMethod getHandlerInternal(HttpServletRequest request) throws Exception {
+        request.getRequestURI().contains('/service/handler/mapping') ? new HandlerMethod('', 'toString') : null
+    }
 }
 
 @Component
 class AlwaysFalseRequestMappingHandlerAdapter extends RequestMappingHandlerAdapter {
     @Override
     protected boolean supportsInternal(HandlerMethod handlerMethod) {
-        super.supportsInternal(handlerMethod) && false
+        false
     }
 }
