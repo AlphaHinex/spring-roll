f9f254a66d91075a75aa57ceb206e0aa8fd45171
Alpha Hinex <AlphaHinex@gmail.com>
2022-04-27 10:33:08 +0800

    Make test pass

--- modules/blocks/roll-utils/src/test/groovy/io/github/springroll/utils/http/HttpClientSpec.groovy
+++ modules/blocks/roll-utils/src/test/groovy/io/github/springroll/utils/http/HttpClientSpec.groovy
@@ -21,21 +21,21 @@ class HttpClientSpec extends Specification {
         HttpClient.post(url, MediaType.get('application/x-www-form-urlencoded'), data).code() == 200
         HttpClient.post(url, headers, MediaType.get('application/x-www-form-urlencoded'), data).code() == 200
         HttpClient.put(url, MediaType.get('application/json'), data).code() == 500
-//        HttpClient.put(url, headers, MediaType.APPLICATION_JSON, data).statusCode == HttpStatus.INTERNAL_SERVER_ERROR
-//        HttpClient.get(url).statusCode == HttpStatus.OK
-//        HttpClient.get(url, headers).statusCode == HttpStatus.OK
-//        HttpClient.delete(url, MediaType.APPLICATION_FORM_URLENCODED, data).statusCode == HttpStatus.INTERNAL_SERVER_ERROR
-//        HttpClient.delete(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data).statusCode == HttpStatus.INTERNAL_SERVER_ERROR
-//        HttpClient.delete(url).statusCode == HttpStatus.INTERNAL_SERVER_ERROR
-//        HttpClient.delete(url, headers).statusCode == HttpStatus.INTERNAL_SERVER_ERROR
+        HttpClient.put(url, MediaType.get('application/json'), data).code() == 500
+        HttpClient.get(url).code() == 200
+        HttpClient.get(url, headers).code() == 200
+        HttpClient.delete(url, MediaType.get('application/x-www-form-urlencoded'), data).code() == 500
+        HttpClient.delete(url, headers, MediaType.get('application/x-www-form-urlencoded'), data).code() == 500
+        HttpClient.delete(url).code() == 500
+        HttpClient.delete(url, headers).code() == 500
     }
 
     def "Could get stream"() {
         def r = HttpClient.get("$TEAMCITY/img/collapse.png")
 
         expect:
-        r.getBody() != null
-        r.getHeaders().getContentType() == MediaType.IMAGE_PNG
+        r.body() != null
+        r.headers().get('Content-Type') == 'image/png'
     }
 
     def "Async request with callback"() {
@@ -58,20 +58,10 @@ class HttpClientSpec extends Specification {
         }
 
         expect:
-        HttpClient.post("$TEAMCITY/login.html", MediaType.APPLICATION_FORM_URLENCODED, '{"user":"123"}', cb)
-        HttpClient.post('https://www.google.com', MediaType.APPLICATION_FORM_URLENCODED, '{"user":"123"}', 200, cb)
+        HttpClient.post("$TEAMCITY/login.html", MediaType.get('application/x-www-form-urlencoded'), '{"user":"123"}', cb)
+        HttpClient.post('https://www.google.com', MediaType.get('application/x-www-form-urlencoded'), '{"user":"123"}', 200, cb)
         HttpClient.get('http://localhost:9090/pep', 200, cb)
         latch.await(1, TimeUnit.SECONDS)
-    }
-
-    def "test getFormUrlEncodedData"() {
-        expect:
-        result.toLowerCase() == HttpClient.getFormUrlEncodedData(input).toLowerCase()
-        where:
-        input                                                    | result
-        ['key1': 'aaa']                                          | 'key1=aaa'
-        ['key1': 'aaa', 'zongwen': '这个是中文']                      | 'key1=aaa&zongwen=%e8%bf%99%e4%b8%aa%e6%98%af%e4%b8%ad%e6%96%87'
-        ['key1': 'aaa', 'url': 'http://www.baidu.com?a1=1&a2=2'] | 'key1=aaa&url=http%3a%2f%2fwww.baidu.com%3fa1%3d1%26a2%3d2'
     }
 
     @Ignore
@@ -96,42 +86,42 @@ class HttpClientSpec extends Specification {
 
         when:
         def postStart = new Date().getTime()
-        HttpClient.post(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data)
+        HttpClient.post(url, headers, MediaType.get('application/x-www-form-urlencoded'), data)
         then:
         thrown(ConnectException)
         def postSpendTime = new Date().getTime() - postStart
 
         when:
         postStart = new Date().getTime()
-        HttpClient.post(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data, 1)
+        HttpClient.post(url, headers, MediaType.get('application/x-www-form-urlencoded'), data, 1)
         then:
         thrown(ConnectException)
         assert postSpendTime < new Date().getTime() - postStart
 
         when:
         def putStart = new Date().getTime()
-        HttpClient.put(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data)
+        HttpClient.put(url, headers, MediaType.get('application/x-www-form-urlencoded'), data)
         then:
         thrown(ConnectException)
         def putSpendTime = new Date().getTime() - putStart
 
         when:
         putStart = new Date().getTime()
-        HttpClient.put(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data, 1)
+        HttpClient.put(url, headers, MediaType.get('application/x-www-form-urlencoded'), data, 1)
         then:
         thrown(ConnectException)
         assert putSpendTime < new Date().getTime() - putStart
 
         when:
         def deleteStart = new Date().getTime()
-        HttpClient.delete(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data)
+        HttpClient.delete(url, headers, MediaType.get('application/x-www-form-urlencoded'), data)
         then:
         thrown(ConnectException)
         def deleteSpendTime = new Date().getTime() - deleteStart
 
         when:
         deleteStart = new Date().getTime()
-        HttpClient.delete(url, headers, MediaType.APPLICATION_FORM_URLENCODED, data, 1)
+        HttpClient.delete(url, headers, MediaType.get('application/x-www-form-urlencoded'), data, 1)
         then:
         thrown(ConnectException)
         assert deleteSpendTime < new Date().getTime() - deleteStart
