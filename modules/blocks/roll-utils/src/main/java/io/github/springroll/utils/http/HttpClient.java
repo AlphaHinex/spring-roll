223260f28d1725e62709e4a02b6dc30d1338f23d
Alpha Hinex <AlphaHinex@gmail.com>
2022-04-27 10:40:23 +0800

    Clean code

--- modules/blocks/roll-utils/src/main/java/io/github/springroll/utils/http/HttpClient.java
+++ modules/blocks/roll-utils/src/main/java/io/github/springroll/utils/http/HttpClient.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
