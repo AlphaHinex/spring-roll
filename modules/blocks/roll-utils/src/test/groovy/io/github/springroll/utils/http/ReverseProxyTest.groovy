1f3c8f298a5381edbd3b269f6383cf7e4f4dd91a
Alpha Hinex <AlphaHinex@gmail.com>
2022-08-24 16:37:19 +0800

    Add unit test (#45)
    * Prepare put with multipart file test
    
    * Prepare to simulate problem request
    
    * Clean code

--- modules/blocks/roll-utils/src/test/groovy/io/github/springroll/utils/http/ReverseProxyTest.groovy
+++ modules/blocks/roll-utils/src/test/groovy/io/github/springroll/utils/http/ReverseProxyTest.groovy
@@ -47,4 +47,26 @@ class ReverseProxyTest extends AbstractIntegrationTest {
         assert response.status == HttpStatus.NO_CONTENT.value()
     }
 
+    @Test
+    void proxyMultipart() {
+        // redirect /echo/api/echo to ${getPrefix()}/test/proxy/echo
+        MockHttpServletRequest request = new MockHttpServletRequest('PUT', '/echo/api/echo')
+
+        request.setContentType('multipart/form-data; boundary=----WebKitFormBoundaryrJ4Vyb6pAOqbCZQK')
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream()
+        os.write('------WebKitFormBoundaryrJ4Vyb6pAOqbCZQK\r\n'.bytes)
+        os.write('Content-Disposition: form-data; name="s2ibinary"; filename="springweb-1.0.txt\r\n'.bytes)
+        os.write('Content-Type: application/octet-stream\r\n\r\n'.bytes)
+        os.write('test content'.bytes)
+        os.write('\r\n------WebKitFormBoundaryrJ4Vyb6pAOqbCZQK--\r\n'.bytes)
+        request.setContent(os.toByteArray())
+
+        HttpServletResponse response = new MockHttpServletResponse()
+        ReverseProxy.proxyPass(request, response, "${getPrefix()}/test/proxy", '/echo/api', null)
+
+        assert response.status == HttpStatus.OK.value()
+        assert response.contentAsString == 'TRUE'
+    }
+
 }
