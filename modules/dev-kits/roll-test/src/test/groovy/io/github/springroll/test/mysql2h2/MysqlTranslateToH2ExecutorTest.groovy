cbfc2219097099d61bebd01312f0355e9a283396
Alpha Hinex <AlphaHinex@gmail.com>
2023-12-14 15:45:22 +0800

    Check data count after create table

--- modules/dev-kits/roll-test/src/test/groovy/io/github/springroll/test/mysql2h2/MysqlTranslateToH2ExecutorTest.groovy
+++ modules/dev-kits/roll-test/src/test/groovy/io/github/springroll/test/mysql2h2/MysqlTranslateToH2ExecutorTest.groovy
@@ -2,14 +2,19 @@ package io.github.springroll.test.mysql2h2
 
 import io.github.springroll.test.AbstractSpringTest
 import org.junit.Test
-import org.springframework.context.annotation.Profile
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
 
-@Profile('test')
 class MysqlTranslateToH2ExecutorTest extends AbstractSpringTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate
 
     @Test
     void test() {
-        assert true
+        def map = jdbcTemplate.queryForMap('select count(*) from champ_app_mgr_d')
+        assert map.size() == 1
+        assert map['COUNT(*)'] == 0
     }
 
 }
